42511c1aa9496630d6ca7ef7c08d0bf5779653b4
Yarek <jrslwlkn@gmail.com>
2020-03-30 13:49:29 -0500

    initial commit; working prototype

---
+++ main.js
@@ -0,0 +1,67 @@
+const getVideosPlaylistPage = () => {
+    const playlist = document.querySelector('ytd-playlist-video-list-renderer.ytd-item-section-renderer')
+    if (!playlist) return;
+
+    const contents = playlist.querySelector('#contents')
+    const videos = contents.querySelectorAll('span.ytd-thumbnail-overlay-time-status-renderer')
+
+    return videos.length ? videos : null;
+}
+
+const getVideosRegularPage = () => {
+    const playlist = document.getElementById('playlist')
+    if (!playlist) return;
+
+    const items = playlist.querySelector('#items')
+    const videos = items.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')
+
+    return videos.length ? videos : null;
+}
+
+const getPlaylistLength = videos => {
+    const durations = []; 
+    videos.forEach(v => durations.push(v.innerText))
+
+    const convertTimestringToSeconds = duration => {
+        duration = duration
+            .split(':')
+            .map(t => +t)
+            .filter(t => t)
+
+        while (duration.length > 1) {
+            duration[1] += duration[0] * 60;
+            duration = duration.slice(1)
+        }
+        return duration[0];
+    }
+
+    const totalSecs = durations.reduce((acc, cur) => acc + convertTimestringToSeconds(cur), 0)
+
+    const getPreparedDuration = duration => {
+        const hours = parseInt(duration/3600)
+        const minutes = parseInt((duration - hours*3600)/60)
+        const seconds = duration - hours*3600 - minutes*60;
+
+        return [seconds, minutes, hours].filter(x => x)
+    }
+
+    const getPrettyOutput = duration => {
+        const labels = ['second', 'minute', 'hour']
+
+        return duration
+            .map((t, i) => t ? t > 1 ? `${t} ${labels[i]}s` : `${t} ${labels[i]}` : null)
+            .filter(x => x)
+            .reverse()
+            .join(' ')
+
+    }
+
+    return getPrettyOutput(getPreparedDuration(totalSecs));
+}
+
+const isPlaylistPage = () => window.location.pathname.slice(1) === 'playlist';
+
+const data = isPlaylistPage ? getVideosPlaylistPage() : getVideosRegularPage();
+if (data) {
+    console.log(getPlaylistLength(data))
+}
